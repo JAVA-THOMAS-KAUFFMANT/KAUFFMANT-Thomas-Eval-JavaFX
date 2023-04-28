82ed99966f8979472202deb8d7850f9d00e78874
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-28 10:23:44 +0200

    feat: READ API

--- src/main/java/com/example/ds/HelloApplication.java
+++ src/main/java/com/example/ds/HelloApplication.java
@@ -9,6 +9,17 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class HelloApplication extends Application {
 
     Stage showDataStage;
@@ -58,10 +69,19 @@ public class HelloApplication extends Application {
             Label downloadDataTitle = new Label("Download Data :");
             downloadDataContainer.getChildren().addAll(downloadDataTitle);
 
+            JSONObject json = null;
+            try {
+                json = readJsonFromUrl("https://download.data.grandlyon.com/ws/grandlyon/pvo_patrimoine_voirie.pvostationvelov/all.json?maxfeatures=-1");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            System.out.println(json.toString());
+            System.out.println(json.get("id"));
+
             Scene downloadDataScene = new Scene(downloadDataContainer, 400, 400);
-            deleteDataStage = new Stage();
-            deleteDataStage.setScene(downloadDataScene);
-            deleteDataStage.show();
+            downloadDataStage = new Stage();
+            downloadDataStage.setScene(downloadDataScene);
+            downloadDataStage.show();
         });
 
         // => EventListener SuggestDataButton
@@ -98,9 +118,30 @@ public class HelloApplication extends Application {
         container.getChildren().addAll(title, showData, downloadData, suggestData, deleteSuggest);
 
         // Set scene
-        Scene mainScene = new Scene(container, 400, 400);
+        Scene mainScene = new Scene(container, 600, 600);
         primaryStage.setScene(mainScene);
         primaryStage.show();
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ds {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
     opens com.example.ds to javafx.fxml;
