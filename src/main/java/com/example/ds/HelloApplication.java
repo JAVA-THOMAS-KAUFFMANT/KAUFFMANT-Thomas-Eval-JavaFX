157304bd07f75f00144eb54f942b1b22196ce5d6
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-28 09:16:35 +0200

    first commit

---
+++ src/main/java/com/example/ds/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.example.ds;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
